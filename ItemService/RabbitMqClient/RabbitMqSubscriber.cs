9e33ba240553442d8a3d34358f0b9743ae2312bb
vinicius00franco <vincus.franco.silva@gmail.com>
2024-12-22 16:14:19 -0300

    iniciando prejeto com microsservicos com dotnet

--- ItemService/RabbitMqClient/RabbitMqSubscriber.cs
+++ ItemService/RabbitMqClient/RabbitMqSubscriber.cs
@@ -16,11 +16,22 @@ namespace ItemService.RabbitMqClient
         public RabbitMqSubscriber(IConfiguration configuration, IProcessaEvento processaEvento)
         {
             _configuration = configuration;
-            _connection = new ConnectionFactory() { HostName = _configuration["RabbitMqHost"], Port = Int32.Parse(_configuration["RabbitMqPort"]) }.CreateConnection();
+            _connection = new ConnectionFactory()
+            {
+                HostName = _configuration["RabbitMqHost"],
+                Port = Int32.Parse(_configuration["RabbitMqPort"])
+            }.CreateConnection();
+
             _channel = _connection.CreateModel();
-            _channel.ExchangeDeclare(exchange: "trigger", type: ExchangeType.Fanout);
+            _channel.ExchangeDeclare(
+                exchange: "trigger", type: ExchangeType.Fanout
+                );
             _nomeDaFila = _channel.QueueDeclare().QueueName;
-            _channel.QueueBind(queue: _nomeDaFila, exchange: "trigger", routingKey: "");
+
+            _channel.QueueBind(
+                queue: _nomeDaFila, exchange: "trigger", routingKey: ""
+                );
+
             _processaEvento = processaEvento;
         }
 
